e8044cffb55f4e25cd725b1e636e9f0dd3f3ddd1
fbouras-nova <farid.bouras@nova-technology.fr>
2023-01-23 17:30:42 +0100

    feat: component iconcard +style

---
+++ src/components/IconCard/IconCard.js
@@ -0,0 +1,13 @@
+import logoSportSee from '../../assets/logo/logoSportSee.png';
+
+import './iconcard.css';
+
+function IconCard() {
+  return (
+    <div>
+      <img className='icon-leftmenu' src={logoSportSee} alt='logo SportSee' />
+    </div>
+  );
+}
+
+export default IconCard;
