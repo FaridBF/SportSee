9eaf712c22a7471f47aba6dee08c6bc708f15a7d
fbouras-nova <farid.bouras@nova-technology.fr>
2023-01-23 15:42:03 +0100

    feat: structure app + dependence browser router

--- src/App.js
+++ src/App.js
@@ -1,20 +1,16 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+// import Page404 from './pages/Page404/Page404';
 
-import About from './pages/About/About';
-import AccommodationSheet from './pages/AccommodationSheet/AccommodationSheet';
-import Home from './pages/Home/Home';
-import Page404 from './pages/Page404/Page404';
+// import { library } from '@fortawesome/fontawesome-svg-core';
+// import { faCopyright } from '@fortawesome/free-regular-svg-icons';
+// import { faStar } from '@fortawesome/free-solid-svg-icons';
+// import {
+//   faChevronDown,
+//   faChevronRight,
+//   faChevronLeft
+// } from '@fortawesome/free-solid-svg-icons';
 
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { faCopyright } from '@fortawesome/free-regular-svg-icons';
-import { faStar } from '@fortawesome/free-solid-svg-icons';
-import {
-  faChevronDown,
-  faChevronRight,
-  faChevronLeft
-} from '@fortawesome/free-solid-svg-icons';
-
-library.add(faCopyright, faStar, faChevronDown, faChevronRight, faChevronLeft);
+// library.add(faCopyright, faStar, faChevronDown, faChevronRight, faChevronLeft);
 
 /**
  * Gestion de nos routes via BrowserRouter (react-router-dom)
@@ -25,13 +21,8 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<Home />} />
-        <Route path='/about' element={<About />} />
-        <Route
-          path='/accommodationSheet/:uid'
-          element={<AccommodationSheet />}
-        />
-        <Route path='*' element={<Page404 />} />
-        <Route path='/error' element={<Page404 />} />
+        {/* <Route path='*' element={<Page404 />} />
+        <Route path='/error' element={<Page404 />} /> */}
       </Routes>
     </BrowserRouter>
   );
