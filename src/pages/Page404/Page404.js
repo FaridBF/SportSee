0c085ddced3f51169a133fa4f735e7e0781a2f30
fbouras-nova <farid.bouras@nova-technology.fr>
2023-01-23 16:40:24 +0100

    feat: add page 404

---
+++ src/pages/Page404/Page404.js
@@ -0,0 +1,24 @@
+import './page404.css';
+
+/**
+ * La page 404 est renvoyée pour chaque route inexistante, ou si une
+ * valeur présente dans l’URL ne fait pas partie des données renseignées
+ */
+function Page404() {
+  return (
+    <>
+      <div className='container-error404'>
+        <h1 className='title-error404'>404</h1>
+        <h2 className='description-error404'>
+          Oups! La page que vous demandez n'existe pas. Elle est en cours de
+          réalisation
+        </h2>
+        <a className='link-home' href='/'>
+          <p>Retourner sur la page d'accueil</p>
+        </a>
+      </div>
+    </>
+  );
+}
+
+export default Page404;
