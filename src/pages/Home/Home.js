1e42c76ec7acfb138105b1984e1434f4ccec26cb
fbouras-nova <farid.bouras@nova-technology.fr>
2023-01-23 16:40:13 +0100

    feat: add home page

---
+++ src/pages/Home/Home.js
@@ -0,0 +1,15 @@
+import Header from '../../components/Header/Header';
+
+import '../Home/home.css';
+
+function Home(props) {
+  return (
+    <div>
+      <Header />
+      <h1>Bonjour Farid</h1>
+      <p>Félécitation! Vous avez explosé vos objectifs hier</p>
+    </div>
+  );
+}
+
+export default Home;
