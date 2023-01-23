13745f316b6a261bdecc3c99e8eca9988d05aba9
fbouras-nova <farid.bouras@nova-technology.fr>
2023-01-23 17:31:10 +0100

    feat: component home+style

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,14 +1,20 @@
 import Header from '../../components/Header/Header';
+import LeftMenu from '../../components/LeftMenu/LeftMenu';
 
 import '../Home/home.css';
 
 function Home(props) {
   return (
-    <div>
+    <section>
       <Header />
-      <h1>Bonjour Farid</h1>
-      <p>Félécitation! Vous avez explosé vos objectifs hier</p>
-    </div>
+      <section className='container'>
+        <LeftMenu />
+        <div className='content-container'>
+          <h1>Bonjour Farid</h1>
+          <p>Félécitation! Vous avez explosé vos objectifs hier</p>
+        </div>
+      </section>
+    </section>
   );
 }
 
