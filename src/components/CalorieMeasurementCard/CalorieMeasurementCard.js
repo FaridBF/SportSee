58ff52cf69f80aaa9b80411a6fa32ac89873d157
fbouras-nova <farid.bouras@nova-technology.fr>
2023-01-30 12:25:06 +0100

    feat: add component calorie-measurement-card

---
+++ src/components/CalorieMeasurementCard/CalorieMeasurementCard.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import './caloriemeasurementcard.css';
+
+function CalorieMeasurementCard(props) {
+  return <div className='calorie-measurement-card'>CalorieMeasurementCard</div>;
+}
+
+export default CalorieMeasurementCard;
