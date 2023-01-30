c61921f412914aa37e38ce518d8a905432ec8c1b
fbouras-nova <farid.bouras@nova-technology.fr>
2023-01-30 12:25:22 +0100

    feat: add component daily-activity

---
+++ src/components/DailyActivity/DailyActivity.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './dailyactivity.css';
+
+function DailyActivity(props) {
+  return <div className='daily-activity-container'>DailyActivity</div>;
+}
+
+export default DailyActivity;
