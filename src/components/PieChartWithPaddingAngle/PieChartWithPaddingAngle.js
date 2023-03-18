9215f39c14a31c4b7629613372603b41ffd6598d
fbouras-nova <farid.bouras@nova-technology.fr>
2023-03-18 18:04:33 +0100

    feat: add piechart with data

--- src/components/PieChartWithPaddingAngle/PieChartWithPaddingAngle.js
+++ src/components/PieChartWithPaddingAngle/PieChartWithPaddingAngle.js
@@ -2,18 +2,19 @@ import React, { PureComponent } from 'react';
 import {
   PieChart,
   Pie,
+  RadialBarChart,
+  RadialBar,
   Sector,
   Cell,
+  Text,
   ResponsiveContainer,
-  Label
+  Label,
+  Legend
 } from 'recharts';
 import DataModelisationService from '../../services/serviceModelisationData';
 
-const data = [
-  { name: 'Group A', value: 100 },
-  { name: 'Group B', value: 300 }
-];
-const COLORS = ['white', 'red', 'red', 'white'];
+// const data = [{ value: 100 }, { value: 100 }];
+// const COLORS = ['white', 'red'];
 
 /**
  * PieChartWithPaddingAngle component recharts
@@ -23,10 +24,33 @@ const COLORS = ['white', 'red', 'red', 'white'];
 export default class PieChartWithPaddingAngle extends PureComponent {
   render() {
     const { userInfos } = this.props;
+    console.log('userInfos', userInfos);
+
+    const data = [
+      { name: 'completed', value: userInfos.data.score, fillColor: 'red' },
+      {
+        name: 'not-completed',
+        value: 1 - userInfos.data.score,
+        fillColor: 'transparent'
+      }
+    ];
+
+    const formatPercent = (value) => `${(value * 100).toFixed(0)}%`;
 
     return (
       <>
-        <p className='scoring'>Score</p>
+        <p>Score avec RadialBarChart</p>
+        {/* PIIIIEEEEE */}
+        {/* <Text
+          style={{
+            color: 'black',
+            fontWeight: 'bold',
+            paddingLeft: '35px'
+          }}
+        >
+          Score
+        </Text> */}
+        {/* <p>Score</p>
         <PieChart width={800} height={400} onMouseEnter={this.onPieEnter}>
           <Pie
             data={data}
@@ -47,21 +71,39 @@ export default class PieChartWithPaddingAngle extends PureComponent {
               />
             ))}
             <Label
-              value={
-                DataModelisationService.converToPercent(userInfos.data.score) +
-                ' de votre objectif'
-              }
+              value={DataModelisationService.converToPercent(
+                userInfos.data.score
+              )}
               position='center'
               className='chart-label'
             />
+            <Label value={'de votre objectif'} position='center' />
             {data.map((entry, index) => (
               <Sector
                 key={`sector-${index}`}
                 value={entry.value}
                 fill={COLORS[index % COLORS.length]}
-              >
-                <Label>{entry.name}</Label>
-              </Sector>
+              ></Sector>
+            ))}
+          </Pie>
+        </PieChart> */}
+        {/* <PieChart width={160} height={160}></PieChart> */}
+        <PieChart width={250} height={220}>
+          <Pie
+            data={data}
+            dataKey='value'
+            innerRadius={70}
+            outerRadius={80}
+            startAngle={90}
+            endAngle={450}
+            cy={120}
+          >
+            {data.map((entry, index) => (
+              <Cell
+                key={`cell-${index}`}
+                fill={entry.fillColor}
+                cornerRadius='50%'
+              />
             ))}
           </Pie>
         </PieChart>
